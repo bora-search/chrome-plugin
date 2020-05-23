d960fd84dde9c1bdea5ff8820bdee33745175e6d
Albert Barreto <albert.barreto@outlook.com>
2020-05-23 01:07:29 -0300

    search engines information

--- js/bora.js
+++ js/bora.js
@@ -1,6 +1,8 @@
 // Store percentage values when user makes changes
 document.addEventListener('DOMContentLoaded', function () {
 
+    $('[data-toggle="popover"]').popover();
+
     Object.keys(SEARCH_ENGINE_URLS).forEach((engine) => {
         let input = document.getElementById(engine);
 
