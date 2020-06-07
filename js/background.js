bbd726715c3af02e6c254cd97941d68faaf584b3
Laurent Ribiere <lribiere@octo.com>
2020-06-07 14:53:47 +0200

    removed omnibox keyword behaviour + updated version

--- js/background.js
+++ js/background.js
@@ -12,25 +12,3 @@ chrome.runtime.onInstalled.addListener(function (details) {
         chrome.storage.sync.set({'activated': true});
     }
 });
-
-chrome.omnibox.onInputEntered.addListener(function (text) {
-    chrome.storage.sync.get('search_engine_distrib_store', function (data) {
-        let searchEngineDistrib = data.search_engine_distrib_store
-        let searchEngineDomains = {};
-        let totalWeight = 0;
-
-        for (const [engine, weight] of Object.entries(searchEngineDistrib)) {
-            let domainStart = totalWeight;
-            let domainEnd = domainStart + weight;
-            searchEngineDomains[engine] = [domainStart, domainEnd];
-            totalWeight += weight;
-        }
-
-        console.log(searchEngineDomains);
-        let rand = Math.random() * 100;
-        let selectedEngine = getEngineFromRand(searchEngineDomains, rand);
-        let searchURL = SEARCH_ENGINE_URLS[selectedEngine] + encodeURIComponent(text);
-        console.log('Rand :', rand, '- selected engine:', selectedEngine);
-        chrome.tabs.update({url: searchURL});
-    });
-});
